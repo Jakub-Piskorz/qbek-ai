0117873d7f2fc06695ff8d39507544562d9cba7b
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-09-19 22:26:34 +0200

    small patch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
 const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
@@ -11,15 +12,16 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
   if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
+    msg.channel.send(`type: ${typeof msg.content}`);
+    console.log("Command: ping");
   } else if (msg.content.startsWith('!kick')) {
     if (msg.mentions.users.size) {
       const taggedUser = msg.mentions.users.first();
       msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+      console.log("Command: tried to kick "+taggedUser.username);
     } else {
       msg.reply('Please tag a valid user!');
+      console.log("Command: wrong user");
     }
   }
 });
