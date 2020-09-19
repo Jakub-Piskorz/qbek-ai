40feecd1da2328a78f60693ff35041548867072f
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-09-19 23:19:12 +0200

    test

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
 
-bot.on('ready', () => {
+bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.channel.send(`type: ${typeof msg.content}`);
-    console.log("Command: ping");
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-      console.log("Command: tried to kick "+taggedUser.username);
-    } else {
-      msg.reply('Please tag a valid user!');
-      console.log("Command: wrong user");
+const prefix = "!";
+
+bot.on("message", (msg) => {
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+  const args = msg.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "test") {
+    if (!args.length) {
+      return msg.channel.send(`No arguments in your command, ${msg.author}!`);
     }
+    msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
   }
 });
