d634615fe71a3c3839063f93161579b12f7ad0c8
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-09-20 01:09:45 +0200

    PM auto message

--- index.js
+++ index.js
@@ -10,10 +10,20 @@ bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
+// ======== LOGGED IN =========
+
 const prefix = "!";
 
 bot.on("message", (msg) => {
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+  if (msg.channel.type === "dm" && !msg.content.startsWith(prefix)) {
+    if (!msg.author.bot) {
+      msg.reply(
+        `Jestem botem, przestań do mnie pisać ${msg.author} =.=\n` +
+          "Pisz do <@156086694611910656> ."
+      );
+      bot.user.setUsername("qbek AI");
+    } else return;
+  }
 
   const args = msg.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
@@ -21,7 +31,9 @@ bot.on("message", (msg) => {
   if (command === "test") {
     if (!args.length) {
       return msg.channel.send(`No arguments in your command, ${msg.author}!`);
-    }
-    msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    } else
+      msg.channel.send(
+        `Command name: ${command}\nArguments: ${args}\nChannel: ${msg.channel.type}`
+      );
   }
 });
