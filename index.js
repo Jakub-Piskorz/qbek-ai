7918321cd4323a32cfbc26e1f8c4f63995c38aba
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-09-20 23:24:06 +0200

    Added roles based on message reactions

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const TOKEN = process.env.TOKEN;
 
+const autoReply = require("./commands/autoreply.js");
+
 client.login(TOKEN);
 
 client.on("ready", () => {
@@ -43,6 +45,73 @@ client.on("message", (msg) => {
       :flag_gb: - English speedrun
       :flag_jp: - 日本語 speedrun`
     );
-    msg.channel.send(embed);
+
+    const emojiFilter = (reaction, user) => {
+      return ["💻", "🇬🇧", "🇯🇵"].includes(reaction.emoji.name) || true;
+    };
+
+    msg.channel.send(embed).then((board) => {
+      board
+        .react("💻")
+        .then(() => board.react("🇬🇧"))
+        .then(() => board.react("🇯🇵"));
+    });
+  }
+});
+client.on("messageReactionAdd", (reaction, user) => {
+  if (user.bot) return;
+  if (
+    reaction.message.author.bot &&
+    reaction.message.channel.id === "756705273921405009"
+  ) {
+    const webDevRunner = reaction.message.guild.roles.find(
+      (role) => role.name === "Web dev runner"
+    );
+    const englishRunner = reaction.message.guild.roles.find(
+      (role) => role.name === "English runner"
+    );
+    const japaneseRunner = reaction.message.guild.roles.find(
+      (role) => role.name === "日本語 runner"
+    );
+    const foundMember = reaction.message.guild.members.find(
+      (m) => m.user.username === user.username
+    );
+
+    if (reaction.emoji.name === "💻") {
+      foundMember.addRole(webDevRunner).catch((err) => console.error(err));
+    } else if (reaction.emoji.name === "🇬🇧") {
+      foundMember.addRole(englishRunner).catch((err) => console.error(err));
+    } else if (reaction.emoji.name === "🇯🇵") {
+      foundMember.addRole(japaneseRunner).catch((err) => console.error(err));
+    }
+  }
+});
+
+client.on("messageReactionRemove", (reaction, user) => {
+  if (user.bot) return;
+  if (
+    reaction.message.author.bot &&
+    reaction.message.channel.id === "756705273921405009"
+  ) {
+    const webDevRunner = reaction.message.guild.roles.find(
+      (role) => role.name === "Web dev runner"
+    );
+    const englishRunner = reaction.message.guild.roles.find(
+      (role) => role.name === "English runner"
+    );
+    const japaneseRunner = reaction.message.guild.roles.find(
+      (role) => role.name === "日本語 runner"
+    );
+    const foundMember = reaction.message.guild.members.find(
+      (m) => m.user.username === user.username
+    );
+
+    if (reaction.emoji.name === "💻") {
+      foundMember.removeRole(webDevRunner).catch((err) => console.error(err));
+    } else if (reaction.emoji.name === "🇬🇧") {
+      foundMember.removeRole(englishRunner).catch((err) => console.error(err));
+    } else if (reaction.emoji.name === "🇯🇵") {
+      foundMember.removeRole(japaneseRunner).catch((err) => console.error(err));
+    }
   }
 });
